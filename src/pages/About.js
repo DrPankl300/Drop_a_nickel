4f8236461cabf439b471045b5e5380add1586e7c
abyer422 <abyer422@gmail.com>
2022-03-16 13:08:26 -0600

    added about me

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,14 +10,28 @@ function About() {
 			</div>
 			<div className="RS">
 				<h1> ABOUT ME </h1>
-				<p> In nulla labore cillum ut culpa irure eu velit sed exercitation aute esse fugiat dolore 
- 					roident aliquip pariatur fugiat. Aute in ut minim amet duis ut nulla ut in officia 
- 					dolore voluptate ullamco sunt in aute labore in ea sit. Lorem ipsum mollit magna laboris 
- 					consectetur magna ut ex ut occaecat culpa in aliquip nulla deserunt sint nostrud laborum 
- 					tempor qui consectetur fugiat dolore do amet laboris est fugiat mollit ut labore 
- 					adipisicing deserunt velit non exercitation elit elit esse et velit commodo ex dolore 
- 					amet quis qui et ad laborum.
- 				</p>
+				<p> Hi, I’m Billy Grant, owner and operator of Drop A Nickel Pro Shop! </p>
+
+				<p>I’m 34 years old and have been bowling since I was 4. I shot my first 200 game at age 12 with my plastic Ebonite Maxim and from that point on, I knew that bowling was going to be my life! Since then I shot my first 600 series at age 13, 
+					got my average over 200 at 14, had my first 300 at 15, and fired off my first 800 at 17! I now have 10 800 series and 18 300 games with a high average for a league season at 232. As a youth bowler, I amassed tons of tournament wins, 
+					including 1 national title in Detroit in 2008, earning myself over $20,000 in scholarship. This helped out a great deal in college where I bowled a season for the Wichita State Shockers before coming back home and captaining The UNM 
+					Lobos for another 2 years where I added a couple of individual tournament wins along with being ranked in the top 15 in the country my last year. I currently have my Professional Bowler’s Association membership card and enjoy competing 
+					on the regional tour in the Southwest conference when my schedule allows. My focus these days is less on the competitive side and more on the pro shop side of bowling as my wife Kelly and I prepare to welcome our first child in October 
+					‘22. </p>
+
+				<p>I learned how to drill my own bowling balls when I turned 18 and was offered a position in the pro shop when I was 20. Since then, I have owned and drilled for myself around 300 bowling balls and for others I approximate the number of 
+					bowling balls I have drilled to be at least 10,000 plus! I was promoted to pro shop manager at age 22 and remained in that position until Covid shut down the bowling center in 2020. Expecting to return to the shop in 2021, I was 
+					abruptly informed that the center owners decided to make a change and I would not be coming back to the place I had worked for the last 20 years. With that, I decided to continue providing the best pro shop services in New Mexico, 
+					only this time, as a smaller home-based operation. Currently working out of my new garage set up, I can fit and measure, revive and resurface, or plug and drill your bowling balls, along with any of your other bowling needs. 
+					In the short period of time with this new setup and location, I have already worked on and drilled over 200 bowling balls! </p>
+
+				<p>With a new location, comes a new name. To “drop a nickel” is a bowling term to roll 5 strikes in a row. As a youth bowler, it was my goal to at least drop a nickel every game. If I could drop a nickel, I knew no matter how the rest of 
+					the game went, it should still be a decent game. This turned into a competition with my friends to see who could drop the most nickels during a set or tournament. Now, it’s my goal to provide the best equipment and services possible 
+					to enable you to drop a nickel as well! </p>
+
+				<p>A big thank you to my loyal customers that have stuck with me during this transition period over the last year, I truly appreciate you all and welcome you to DROP A NICKEL PRO SHOP! </p>
+
+				<p>-Billy </p>
 			</div>
 		</div>
 	)
